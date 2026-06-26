ea46169274a0d58d75d44dceea3d06bc173622f1
alin00r <alinoorspam@gmail.com>
2026-06-26 23:44:33 +0300

    Add reusable validation middleware

---
+++ src/middlewares/validate.ts
@@ -0,0 +1,28 @@
+import { Request, Response, NextFunction } from 'express';
+import Joi from 'joi';
+
+export const validate = (schema: Joi.ObjectSchema, property: 'body' | 'query' | 'params' = 'body') => {
+  return (req: Request, res: Response, next: NextFunction): void => {
+    const { error, value } = schema.validate(req[property], {
+      abortEarly: false,
+      stripUnknown: true,
+    });
+
+    if (error) {
+      const errors = error.details.map((detail) => ({
+        field: detail.path.join('.'),
+        message: detail.message,
+      }));
+
+      res.status(400).json({
+        success: false,
+        message: 'Validation error',
+        errors,
+      });
+      return;
+    }
+
+    req[property] = value;
+    next();
+  };
+};
